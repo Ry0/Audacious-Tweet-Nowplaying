c21a22d434365e8dfd83b290bf3305e2042a221f
Ry0 <k104037r@mail.kyutech.jp>
2015-04-12 22:35:55 +0900

    各種トークンを別ファイルに

--- aud-nowplaying.py
+++ aud-nowplaying.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python
-#coding utf-8
+# -*- coding: utf-8 -*-
 import os, subprocess, ConfigParser, sys
 from requests_oauthlib import OAuth1Session
 
+config = ConfigParser.ConfigParser()
+config.read(['./aud-nowplaying.conf'])
+
 # 自分で取得したTwitterの各種トークン
-CK = 'abcdefghijklmnopqrstuvwxy'                                  # Consumer Key
-CS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx'         # Consumer Secret
-AT = '123456789-ABCDEFGHIJKLMNOPQRSTUVWXYABCDEFGHIJKLMNO'         # Access Token
-AS = 'abcdefghijklmnopqrstuvwxyabcdefghijklmnopqrst'              # Accesss Token Secert
+CONSUMER_KEY = config.get('connect_params', 'CONSUMER_KEY', 1)               # Consumer Key
+# print CONSUMER_KEY
+CONSUMER_SECRET = config.get('connect_params', 'CONSUMER_SECRET', 1)         # Consumer Secret
+# print CONSUMER_SECRET
+ACCESS_TOKEN_KEY = config.get('connect_params', 'ACCESS_TOKEN_KEY', 1)       # Access Token
+# print ACCESS_TOKEN_KEY
+ACCESS_TOKEN_SECRET = config.get('connect_params', 'ACCESS_TOKEN_SECRET', 1) # Accesss Token Secert
+# print ACCESS_TOKEN_SECRET
 
 # ツイート投稿用のURL
 url = "https://api.twitter.com/1.1/statuses/update.json"
 
 # Audaciousからいま再生している楽曲データを取得
 curr_song = subprocess.Popen(['audtool', 'current-song'], stdout=subprocess.PIPE)
-status = "#nowplaying" + ' ' + curr_song.stdout.read().rstrip()
-
+print "コメントを入力"
+comment = raw_input('>>>  ')
+status = "#nowplaying" + ' ' + curr_song.stdout.read().rstrip() + ' ' + comment
 params = {"status": status}
 
 # OAuth認証で POST method で投稿
-twitter = OAuth1Session(CK, CS, AT, AS)
+twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN_KEY, ACCESS_TOKEN_SECRET)
 req = twitter.post(url, params = params)
 
 # レスポンスを確認
